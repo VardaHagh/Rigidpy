8e6be466e6900a6032ebfbab7e8621f54e143df4
mahdisadjadia <sadjadi.seyedmahdi@gmail.com>
2018-11-13 20:45:17 -0800

    fix a bug in setup file

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ setup(
     version='0.0.2',
     description='rigidpy package for rigidity analysis',
     long_description=readme,
+    keywords='rigidity physics math python flexibility condensedmatter',
     author='Varda Faghir Hagh',
     author_email='vardahagh@uchicago.edu',
-    url='https://github.com/vfaghirh/Rigidpy',
+    url='https://github.com/vfaghirh/rigidpy',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
