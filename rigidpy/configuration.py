b2e7d3ac8adb78385f126c5d7fac10b2f19d7a7b
mahdisadjadia <sadjadi.seyedmahdi@gmail.com>
2018-12-15 12:37:57 -0800

    making the naming scheme more uniform

--- rigidpy/configuration.py
+++ rigidpy/configuration.py
@@ -1,4 +1,4 @@
-bondsfrom __future__ import division, print_function, absolute_import
+from __future__ import division, print_function, absolute_import
 
 import numpy as np
 from .framework import Framework
